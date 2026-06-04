4e96f9670b852d3dc3c8b1966b025af0a5db6462
Henry <henrycalda285@gmail.com>
2026-06-04 22:47:45 +0800

    Login Form links to Applicant Dashboard

--- LoginForm.cs
+++ LoginForm.cs
@@ -115,5 +115,25 @@ namespace HRApplicantWindowSystem
 
             this.Show();
         }
+                    this.Hide(); 
+
+            ApplicantDashboardForm dashboard = new ApplicantDashboardForm();
+            dashboard.ShowDialog(); 
+
+            this.Show(); 
+        }
+        }
+
+        private void btnCreateAccount_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+
+            ApplicantRegisterForm registerForm = new ApplicantRegisterForm();
+            registerForm.ShowDialog();
+
+            this.Show();
+        }
+
     }
 }
