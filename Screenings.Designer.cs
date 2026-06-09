216405069b83f89025026d5d2a22f379b2e239a3
Sheena <labutapsheenamae@gmail.com>
2026-06-10 00:05:11 +0800

    Screening design

---
+++ Screenings.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace HRApplicantWindowSystem
+{
+    partial class Screenings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            panel1 = new Panel();
+            btnLogout = new Button();
+            btnDashboard = new Button();
+            lblWelcome = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label3 = new Label();
+            label2 = new Label();
+            label4 = new Label();
+            EntDescription = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            btnAddVacancy = new Button();
+            button4 = new Button();
+            EntJobtitle = new TextBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(483, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(212, 28);
+            label1.TabIndex = 3;
+            label1.Text = "SCREENING MODULE";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ControlDarkDark;
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(btnLogout);
+            panel1.Controls.Add(btnDashboard);
+            panel1.Controls.Add(lblWelcome);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(305, 649);
+            panel1.TabIndex = 4;
+            // 
+            // btnLogout
+            // 
+            btnLogout.Location = new Point(28, 442);
+            btnLogout.Name = "btnLogout";
+            btnLogout.Size = new Size(228, 62);
+            btnLogout.TabIndex = 6;
+            btnLogout.Text = "Logout";
+            btnLogout.UseVisualStyleBackColor = true;
+            // 
+            // btnDashboard
+            // 
+            btnDashboard.Location = new Point(28, 65);
+            btnDashboard.Name = "btnDashboard";
+            btnDashboard.Size = new Size(228, 62);
+            btnDashboard.TabIndex = 1;
+            btnDashboard.Text = "Dashboard";
+            btnDashboard.UseVisualStyleBackColor = true;
+            // 
+            // lblWelcome
+            // 
+            lblWelcome.AutoSize = true;
+            lblWelcome.ForeColor = SystemColors.HighlightText;
+            lblWelcome.Location = new Point(51, 13);
+            lblWelcome.Name = "lblWelcome";
+            lblWelcome.Size = new Size(99, 20);
+            lblWelcome.TabIndex = 0;
+            lblWelcome.Text = "Welcome HR!";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(28, 144);
+            button1.Name = "button1";
+            button1.Size = new Size(228, 62);
+            button1.TabIndex = 7;
+            button1.Text = "Screening Module";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(28, 222);
+            button2.Name = "button2";
+            button2.Size = new Size(228, 62);
+            button2.TabIndex = 8;
+            button2.Text = "Interview Moduling";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(28, 303);
+            button3.Name = "button3";
+            button3.Size = new Size(228, 62);
+            button3.TabIndex = 9;
+            button3.Text = "Interview Evaluation";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(324, 99);
+            label3.Name = "label3";
+            label3.Size = new Size(201, 25);
+            label3.TabIndex = 8;
+            label3.Text = "Applicant Information";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(324, 239);
+            label2.Name = "label2";
+            label2.Size = new Size(201, 25);
+            label2.TabIndex = 9;
+            label2.Text = "Submitted Documents";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(324, 357);
+            label4.Name = "label4";
+            label4.Size = new Size(174, 25);
+            label4.TabIndex = 10;
+            label4.Text = "Screening Remarks";
+            // 
+            // EntDescription
+            // 
+            EntDescription.ForeColor = SystemColors.WindowFrame;
+            EntDescription.Location = new Point(327, 397);
+            EntDescription.Multiline = true;
+            EntDescription.Name = "EntDescription";
+            EntDescription.Size = new Size(515, 118);
+            EntDescription.TabIndex = 12;
+            EntDescription.Text = "Enter Remarks...";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(327, 133);
+            label5.Name = "label5";
+            label5.Size = new Size(95, 20);
+            label5.TabIndex = 13;
+            label5.Text = "Applicant ID:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(327, 165);
+            label6.Name = "label6";
+            label6.Size = new Size(120, 20);
+            label6.TabIndex = 14;
+            label6.Text = "Applicant Name:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(327, 197);
+            label7.Name = "label7";
+            label7.Size = new Size(121, 20);
+            label7.TabIndex = 15;
+            label7.Text = "Position Applied:";
+            // 
+            // btnAddVacancy
+            // 
+            btnAddVacancy.BackColor = SystemColors.Highlight;
+            btnAddVacancy.FlatStyle = FlatStyle.Flat;
+            btnAddVacancy.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAddVacancy.ForeColor = SystemColors.ButtonHighlight;
+            btnAddVacancy.Location = new Point(403, 563);
+            btnAddVacancy.Name = "btnAddVacancy";
+            btnAddVacancy.Size = new Size(148, 29);
+            btnAddVacancy.TabIndex = 16;
+            btnAddVacancy.Text = "Qualified";
+            btnAddVacancy.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ScrollBar;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.ForeColor = SystemColors.Desktop;
+            button4.Location = new Point(594, 563);
+            button4.Name = "button4";
+            button4.Size = new Size(148, 29);
+            button4.TabIndex = 17;
+            button4.Text = "Not Qualified";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // EntJobtitle
+            // 
+            EntJobtitle.ForeColor = SystemColors.WindowFrame;
+            EntJobtitle.Location = new Point(483, 130);
+            EntJobtitle.Name = "EntJobtitle";
+            EntJobtitle.Size = new Size(326, 27);
+            EntJobtitle.TabIndex = 18;
+            // 
+            // textBox1
+            // 
+            textBox1.ForeColor = SystemColors.WindowFrame;
+            textBox1.Location = new Point(483, 162);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(326, 27);
+            textBox1.TabIndex = 19;
+            // 
+            // textBox2
+            // 
+            textBox2.ForeColor = SystemColors.WindowFrame;
+            textBox2.Location = new Point(483, 194);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(326, 27);
+            textBox2.TabIndex = 20;
+            // 
+            // Screenings
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(854, 649);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(EntJobtitle);
+            Controls.Add(button4);
+            Controls.Add(btnAddVacancy);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(EntDescription);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(panel1);
+            Controls.Add(label1);
+            Name = "Screenings";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Panel panel1;
+        private Button btnLogout;
+        private Button btnDashboard;
+        private Label lblWelcome;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Label label3;
+        private Label label2;
+        private Label label4;
+        private TextBox EntDescription;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Button btnAddVacancy;
+        private Button button4;
+        private TextBox EntJobtitle;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
